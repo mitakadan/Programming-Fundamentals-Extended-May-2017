069236799e4e4511f4f175ba8900f0025e81a3af
Dimitar Danailov <30422152+mitakadan@users.noreply.github.com>
2017-08-05 01:33:10 +0200

    Add files via upload

---
+++ 03.Data-Types-and-Variables-Exercises/03. Exchange Variable Values.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Exchange_Variable_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+
+            var c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
