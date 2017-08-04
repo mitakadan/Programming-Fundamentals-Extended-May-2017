d8f9e205d0a2abf44e9f7ae0abd8345d832e70b8
Dimitar Danailov <30422152+mitakadan@users.noreply.github.com>
2017-08-05 01:26:52 +0200

    Add files via upload

---
+++ 02. Conditional-Statements-and-Loops-Exercises/04. Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var month = Console.ReadLine();
+            var nights = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
